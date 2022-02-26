50d8fb3ff0dc49030e1f00bfc54f56b254b3cfa4
t07902301 <e-well@outlook.com>
2022-02-26 14:43:43 -0700

    modified csv headers

--- inaSpeechSegmenter/export_funcs.py
+++ inaSpeechSegmenter/export_funcs.py
@@ -27,7 +27,8 @@ import pandas as pd
 from pytextgrid.PraatTextGrid import PraatTextGrid, Interval, Tier
 
 def seg2csv(lseg, fout=None):
-    df = pd.DataFrame.from_records(lseg, columns=['labels', 'start', 'stop'])
+    # df = pd.DataFrame.from_records(lseg, columns=['labels', 'start', 'stop'])
+    df = pd.DataFrame.from_records(lseg, columns=['id', 'label'])
     df.to_csv(fout, sep='\t', index=False)
 
 def seg2textgrid(lseg, fout=None):
