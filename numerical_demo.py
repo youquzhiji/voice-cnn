992bd8a1fa043b434055b9e259346380d6e81bbf
t07902301 <e-well@outlook.com>
2022-03-04 22:12:01 -0700

    Add numerical outputs

--- numerical_demo.py
+++ numerical_demo.py
@@ -1,9 +1,9 @@
 '''
-A demo fof getting numerical outputs: (speaker_id,(gender,array(p(female),p(male))))
+A demo fof getting numerical outputs: (speaker_id,(gender,[p(female),p(male)]))
 '''
 # Load the API
 from inaSpeechSegmenter import Segmenter
-root_path='../wav/' #这里是按照vox1的目录结构写的，vox1解压后的文件夹的名字是wav
+root_path='/users/yiwei/voxceleb_trainer/data/wav' #这里是按照vox1的目录结构写的，vox1解压后的文件夹的名字是wav
 wav_files=[]
 import os
 speaker_id = [f for f in os.listdir(root_path)]
@@ -17,4 +17,5 @@ for file in wav_files[:2]: # Only contains two audio files in this demo
     seg_result=seg(file[1])
     pred+=[(id,each) for each in seg_result]
     # print(seg_result)
+print('CNN numerical outputs:')
 print(pred)
